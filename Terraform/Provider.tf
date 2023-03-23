480d31973c369447e98066423b318ec17a0d3e49
Rui Zhu <rayz.zhu@mail.utoronto.ca>
2023-03-23 17:17:57 -0400

    Updating Terraform files/template

--- Terraform/Provider.tf
+++ Terraform/Provider.tf
@@ -15,9 +15,9 @@ resource "aws_vpc" "production-vpc" {
 
 # public subnets
 
-resource "aws_subnets" "public-subnet-1" {
+resource "aws_subnet" "public-subnet-1" {
   cidr_block        = var.public_subnet_1_cidr
-  vpc_id            = aws_production-vpc.id
+  vpc_id            = aws_vpc.production-vpc.id
   availability_zone = var.availability_zones[0]
 
   tags = {
@@ -28,7 +28,7 @@ resource "aws_subnets" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   cidr_block        = var.public_subnet_2_cidr
   vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zone[1]
+  availability_zone = var.availability_zones[1]
 
   tags = {
     Name = "Public subnet 2"
@@ -39,7 +39,7 @@ resource "aws_subnet" "public-subnet-2" {
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = var.private_subnet_1_cidr
   vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zone[2]
+  availability_zone = var.availability_zones[0]
 
   tags = {
     Name = "Private subnet 1"
@@ -49,7 +49,7 @@ resource "aws_subnet" "private-subnet-1" {
 resource "aws_subnet" "private-subnet-2" {
   cidr_block        = var.private_subnet_2_cidr
   vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zone[3]
+  availability_zone = var.availability_zones[1]
 
   tags = {
     Name = "Private subnet 2"
@@ -69,22 +69,22 @@ resource "aws_route_table" "private-route-table" {
 
 # Route table association
 resource "aws_route_table_association" "public-route-1-association" {
-  subnet_id      = aws_subnet.public-subnet-1
+  subnet_id      = aws_subnet.public-subnet-1.id
   route_table_id = aws_route_table.public-route-table.id
 }
 
 resource "aws_route_table_association" "public-route-2-association" {
-  subnet_id      = aws_subnet.public-subnet-2
+  subnet_id      = aws_subnet.public-subnet-2.id
   route_table_id = aws_route_table.public-route-table.id
 }
 
 resource "aws_route_table_association" "private-route-1-association" {
-  subnet_id      = aws_subnet.private-subnet-1
+  subnet_id      = aws_subnet.private-subnet-1.id
   route_table_id = aws_route_table.private-route-table.id
 }
 
 resource "aws_route_table_association" "private-route-2-association" {
-  subnet_id      = aws_subnet.private-subnet-2
+  subnet_id      = aws_subnet.private-subnet-2.id
   route_table_id = aws_route_table.private-route-table.id
 }
 
@@ -120,8 +120,308 @@ resource "aws_internet_gateway" "production-igw" {
 
 # Route the public subnet traffic through the internet Gateway
 resource "aws_route" "public-internet-igw-route" {
-  route_route_table_id   = aws_route_table.public-route-table.id
+  route_table_id         = aws_route_table.public-route-table.id
   gateway_id             = aws_internet_gateway.production-igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+# ALB Security Group (Traffic Internet -> ALB)
+resource "aws_security_group" "load-balancer" {
+  name        = "load_balancer_security_group"
+  description = "Controls access to the ALB"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Security group (traffic ALB -> ECS, ssh -> ECS)
+resource "aws_security_group" "ecs" {
+  name        = "ecs_security_group"
+  description = "Allows inbound access from the ALB only"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.load-balancer.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Production Load Balancer
+resource "aws_lb" "production" {
+  name               = "${var.ecs_cluster_name}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.load-balancer.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+}
+
+# Target group
+resource "aws_alb_target_group" "default-target-group" {
+  name     = "${var.ecs_cluster_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.production-vpc.id
+
+  health_check {
+    path                = var.health_check_path
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
+    matcher             = "200"
+  }
+}
+
+# Listener (redirects traffic from the load balancer to the target group)
+
+
+
+
+# role
+
+resource "aws_iam_role" "ecs-host-role" {
+  name               = "ecs_host_role_prod"
+  assume_role_policy = file("policies/ecs-role.json")
+}
+
+resource "aws_iam_role_policy" "ecs-instance-role-policy" {
+  name   = "ecs_instance_role_policy"
+  policy = file("policies/ecs-instance-role-policy.json")
+  role   = aws_iam_role.ecs-host-role.id
+}
+
+resource "aws_iam_role" "ecs-service-role" {
+  name               = "ecs_service_role_prod"
+  assume_role_policy = file("policies/ecs-role.json")
+}
+
+resource "aws_iam_role_policy" "ecs-service-role-policy" {
+  name   = "ecs_service_role_policy"
+  policy = file("policies/ecs-service-role-policy.json")
+  role   = aws_iam_role.ecs-service-role.id
+}
+
+resource "aws_iam_instance_profile" "ecs" {
+  name = "ecs_instance_profile_prod"
+  path = "/"
+  role = aws_iam_role.ecs-host-role.name
+}
+
+resource "aws_cloudwatch_log_group" "django-log-group" {
+  name              = "/ecs/django-app"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_stream" "django-log-stream" {
+  name           = "django-app-log-stream"
+  log_group_name = aws_cloudwatch_log_group.django-log-group.name
+}
+
+resource "aws_cloudwatch_log_group" "nginx-log-group" {
+  name              = "/ecs/nginx"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_stream" "nginx-log-stream" {
+  name           = "nginx-log-stream"
+  log_group_name = aws_cloudwatch_log_group.nginx-log-group.name
+}
+
+resource "aws_key_pair" "production" {
+  key_name   = "${var.ecs_cluster_name}_key_pair"
+  public_key = file(var.ssh_pubkey_file)
+}
+
+resource "aws_ecs_cluster" "production" {
+  name = "${var.ecs_cluster_name}-cluster"
+}
+
+resource "aws_launch_configuration" "ecs" {
+  name                        = "${var.ecs_cluster_name}-cluster"
+  image_id                    = lookup(var.amis, var.region)
+  instance_type               = var.instance_type
+  security_groups             = [aws_security_group.ecs.id]
+  iam_instance_profile        = aws_iam_instance_profile.ecs.name
+  key_name                    = aws_key_pair.production.key_name
+  associate_public_ip_address = true
+  user_data                   = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}-cluster' > /etc/ecs/ecs.config"
+}
+
+
+
+resource "aws_ecs_service" "production" {
+  name            = "${var.ecs_cluster_name}-service"
+  cluster         = aws_ecs_cluster.production.id
+  task_definition = aws_ecs_task_definition.app.arn
+  iam_role        = aws_iam_role.ecs-service-role.arn
+  desired_count   = var.app_count
+  depends_on      = [aws_alb_listener.ecs-alb-http-listener, aws_iam_role_policy.ecs-service-role-policy]
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+    container_name   = "django-app"
+    container_port   = 8000
+  }
+}
+
+resource "aws_autoscaling_group" "ecs-cluster" {
+  name                 = "${var.ecs_cluster_name}_auto_scaling_group"
+  min_size             = var.autoscale_min
+  max_size             = var.autoscale_max
+  desired_capacity     = var.autoscale_desired
+  health_check_type    = "EC2"
+  launch_configuration = aws_launch_configuration.ecs.name
+  vpc_zone_identifier  = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+}
+
+# RDS Security Group (traffic ECS -> RDS)
+resource "aws_security_group" "rds" {
+  name        = "rds-security-group"
+  description = "Allows inbound access from ECS only"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "5432"
+    to_port         = "5432"
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_subnet_group" "production" {
+  name       = "main"
+  subnet_ids = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+}
+
+resource "aws_db_instance" "production" {
+  identifier              = "production"
+  name                    = var.rds_db_name
+  username                = var.rds_username
+  password                = var.rds_password
+  port                    = "5432"
+  engine                  = "postgres"
+  engine_version          = "12.3"
+  instance_class          = var.rds_instance_class
+  allocated_storage       = "20"
+  storage_encrypted       = false
+  vpc_security_group_ids  = [aws_security_group.rds.id]
+  db_subnet_group_name    = aws_db_subnet_group.production.name
+  multi_az                = false
+  storage_type            = "gp2"
+  publicly_accessible     = false
+  backup_retention_period = 7
+  skip_final_snapshot     = true
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family = "django-app"
+  container_definitions = templatefile("${path.module}/Templates/Django-template.tpl", {
+    docker_image_url_django = var.docker_image_url_django
+    docker_image_url_nginx  = var.docker_image_url_nginx
+    region                  = var.region
+    rds_db_name             = var.rds_db_name
+    rds_username            = var.rds_username
+    rds_password            = var.rds_password
+    rds_hostname            = aws_db_instance.production.address
+    allowed_hosts           = var.allowed_hosts
+  })
+
+  depends_on = [aws_db_instance.production]
+
+  volume {
+    name      = "static_volume"
+    host_path = "/usr/src/app/staticfiles/"
+  }
+}
+
+
+
+# Listener (redirects traffic from the load balancer to the target group)
+resource "aws_alb_listener" "ecs-alb-http-listener" {
+  load_balancer_arn = aws_lb.production.id
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+  depends_on        = [aws_alb_target_group.default-target-group]
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
+}
+
+resource "aws_autoscaling_policy" "alarm-policy" {
+  name                   = "too many request policy"
+  scaling_adjustment     = var.policy_scaled
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.ecs-cluster.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "request-alarm" {
+  alarm_name          = "Too many request"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "RequestCount"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.ecs-cluster.name
+  }
+
+  alarm_description = "This metric monitors ecs request count"
+  alarm_actions     = [aws_autoscaling_policy.alarm-policy.arn]
+}
+
+output "alb_hostname" {
+  value = aws_lb.production.dns_name
+}
+
 
