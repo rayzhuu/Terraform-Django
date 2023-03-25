c2f9f96977752882457076ab55ee2cb04a25b0f7
Rui Zhu <rayz.zhu@mail.utoronto.ca>
2023-03-25 10:10:34 -0400

    Updating to Terragrunt Format, multiple module,DRY

---
+++ Terraform/Prod/Backend/Terragrunt.hcl
@@ -0,0 +1,3 @@
+dependencies {
+  paths = ["../Network", "../ECS", "../RolesPolicy", "../SecurityGroups", "../RDS"]
+}
---
+++ Terraform/Prod/ECS/main.tf
@@ -0,0 +1,165 @@
+# ALB Security Group (Traffic Internet -> ALB)
+resource "aws_security_group" "load-balancer" {
+  name        = "load_balancer_security_group"
+  description = "Controls access to the ALB"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Security group (traffic ALB -> ECS, ssh -> ECS)
+resource "aws_security_group" "ecs" {
+  name        = "ecs_security_group"
+  description = "Allows inbound access from the ALB only"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.load-balancer.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Production Load Balancer
+resource "aws_lb" "production" {
+  name               = "${var.ecs_cluster_name}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.load-balancer.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+}
+
+# Target group
+resource "aws_alb_target_group" "default-target-group" {
+  name     = "${var.ecs_cluster_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.production-vpc.id
+
+  health_check {
+    path                = var.health_check_path
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
+    matcher             = "200"
+  }
+}
+
+resource "aws_ecs_cluster" "production" {
+  name = "${var.ecs_cluster_name}-cluster"
+}
+
+resource "aws_launch_configuration" "ecs" {
+  name                        = "${var.ecs_cluster_name}-cluster"
+  image_id                    = lookup(var.amis, var.region)
+  instance_type               = var.instance_type
+  security_groups             = [aws_security_group.ecs.id]
+  iam_instance_profile        = aws_iam_instance_profile.ecs.name
+  key_name                    = aws_key_pair.production.key_name
+  associate_public_ip_address = true
+  user_data                   = "#!/bin/bash\necho ECS_CLUSTER='${var.ecs_cluster_name}-cluster' > /etc/ecs/ecs.config"
+}
+
+
+
+resource "aws_ecs_service" "production" {
+  name            = "${var.ecs_cluster_name}-service"
+  cluster         = aws_ecs_cluster.production.id
+  task_definition = aws_ecs_task_definition.app.arn
+  iam_role        = aws_iam_role.ecs-service-role.arn
+  desired_count   = var.app_count
+  depends_on      = [aws_alb_listener.ecs-alb-http-listener, aws_iam_role_policy.ecs-service-role-policy]
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+    container_name   = "django-app"
+    container_port   = 8000
+  }
+}
+
+resource "aws_autoscaling_group" "ecs-cluster" {
+  name                 = "${var.ecs_cluster_name}_auto_scaling_group"
+  min_size             = var.autoscale_min
+  max_size             = var.autoscale_max
+  desired_capacity     = var.autoscale_desired
+  health_check_type    = "EC2"
+  launch_configuration = aws_launch_configuration.ecs.name
+  vpc_zone_identifier  = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+}
+
+resource "aws_alb_listener" "ecs-alb-http-listener" {
+  load_balancer_arn = aws_lb.production.id
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+  depends_on        = [aws_alb_target_group.default-target-group]
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family = "django-app"
+  container_definitions = templatefile("${path.module}/Templates/Django-template.tpl", {
+    docker_image_url_django = var.docker_image_url_django
+    docker_image_url_nginx  = var.docker_image_url_nginx
+    region                  = var.region
+    rds_db_name             = var.rds_db_name
+    rds_username            = var.rds_username
+    rds_password            = var.rds_password
+    rds_hostname            = aws_db_instance.production.address
+    allowed_hosts           = var.allowed_hosts
+  })
+
+  depends_on = [aws_db_instance.production]
+
+  volume {
+    name      = "static_volume"
+    host_path = "/usr/src/app/staticfiles/"
+  }
+}
+
+resource "aws_key_pair" "production" {
+  key_name   = "${var.ecs_cluster_name}_key_pair"
+  public_key = file(var.ssh_pubkey_file)
+}
+
